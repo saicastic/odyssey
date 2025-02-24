a2bea1cbeddb6520a9598575d4c26c7d914fae68
saicastic <saiprithvisuperior@gmail.com>
2025-02-24 15:21:14 +0530

    formatted Hero.jsx

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -4,14 +4,10 @@ import NodeCanvas from "../snippets/NodeCanvas";
 import styles from "../styles/sections/Hero.module.css";
 import { useEffect, useRef } from "react";
 
-
 const Hero = () => {
-
   return (
     <section className={styles.heroSec}>
-      <div className={styles.hero}>
-      
-      </div>
+      <div className={styles.hero}></div>
     </section>
   );
 };
